80b56ce89f1304b72f9df3b121d1bbdb99648c29
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-07-23 16:18:01 +1000

    debugoverlays: Display map chunk buffers

--- tool/mesen/debugoverlays.lua
+++ tool/mesen/debugoverlays.lua
@@ -24,6 +24,14 @@ local function createOverlay(margin, scale)
 		margin = margin or 8,
 		surface = emu.drawSurface.scriptHud,
 	}
+	obj.coleru = {
+		0xBF2A7F, 0xE84D5B, 0xF4A854, 0x9A947C, 0xA14016, 0x142026, 0xFF8826, 0xA6E094,
+		0xE7EDEA, 0xFFF8BC, 0x89A194, 0xCC883A, 0xF03813, 0x5B756C, 0xCCAC95, 0x26979F,
+		0xE4E391, 0xEE887A, 0x2C9FA3, 0xF07360, 0x20130A, 0x030D4F, 0x322938, 0xF3214E,
+		0xFFC52C, 0x5C3D5B, 0x9ABC8A, 0xEAE2CF, 0x123142, 0x748B83, 0xE9F0C9, 0xE8E490,
+		0x2D1B33, 0xFB0C06, 0xF36A71, 0x9D7E79, 0x3B3B3B, 0xB4CCB9, 0x3B657A, 0xCEECEF,
+		0xCFC89A, 0xB31237, 0xCF023B, 0xFFB914, 0xFF703F, 0x76BCAD, 0xFA2E59, 0x5C4152,
+	}
 	obj.select = function(self)
 		emu.selectDrawSurface(self.surface, self.scale)
 	end
@@ -61,6 +69,9 @@ local function createOverlay(margin, scale)
 		local x1, y1 = self:getEnd()
 		return x1 - x0, y1 - y0
 	end
+	obj.getSurfaceSize = function(self)
+		return 160 * self.scale, 144 * self.scale
+	end
 	obj.drawLine = function(self, x, y, x2, y2, color, duration, delay)
 		self:select()
 		local ox, oy = self:getOrigin()
@@ -84,21 +95,41 @@ local function createOverlay(margin, scale)
 
 	obj.drawVector = function(self, cx, cy, vx, vy, magMax, displayMag, color, bgcolor)
 		color = color or 0xFF00FF
-		local scale = displayMag / magMax
+		local vecScale = displayMag / magMax
 		local w = displayMag * 2
 		self:drawRectangle(cx - displayMag, cy - displayMag, w, w, bgcolor or 0xA0000000, true)
 		self:drawRectangle(cx - displayMag, cy - displayMag, w, w, color | 0x80000000)
-		self:drawLine(cx, cy, cx + vx * scale, cy + vy * scale, color)
-		self:drawLine(cx, 1 + cy, cx + vx * scale, 1 + cy + vy * scale, color)
-		self:drawLine(1 + cx, cy, 1 + cx + vx * scale, cy + vy * scale, color)
+		self:drawLine(cx, cy, cx + vx * vecScale, cy + vy * vecScale, color)
+		self:drawLine(cx, 1 + cy, cx + vx * vecScale, 1 + cy + vy * vecScale, color)
+		self:drawLine(1 + cx, cy, 1 + cx + vx * vecScale, cy + vy * vecScale, color)
 	end
 
 	obj.drawWindow = function(self)
 		local w, h = self:getSize()
 		self:drawRectangle(0, 0, w, h, 0x0F9BABEB, false)
 	end
-	obj.getSurfaceSize = function(self)
-		return 160 * self.scale, 144 * self.scale
+
+	obj.drawTilemap = function(self, originx, originy, tiles, mapw, maph, tile_size)
+		local boundw = mapw * tile_size
+		local boundh = maph * tile_size
+		self:drawRectangle(originx, originy, boundw, boundh, 0x20606060, true)
+		self:drawRectangle(originx, originy, boundw, boundh, 0x10606060, false)
+		if #tiles ~= mapw * maph then
+			self:drawString(string.format("#tiles(%d) ~= %d", #tiles, mapw*maph))
+			return
+		end
+		for y = 0, maph - 1 do
+			local py = originy + y * tile_size
+			for x = 0, mapw - 1 do
+				local px = originx + x * tile_size
+				local c = tiles[1 + y * mapw + x]
+				if c > 0 and c <= #self.coleru then
+					self:drawRectangle(px, py, tile_size, tile_size, self.coleru[c], true)
+				elseif c ~= 0 then
+					self:drawString(px, py, tostring(c))
+				end
+			end
+		end
 	end
 
 	return obj
@@ -153,7 +184,7 @@ local function st_read(st, addr, memType, ident)
 	data._addr = addr
 	data._memType = memType
 	data._ident = ident
-	for i,field in ipairs(st) do
+	for _, field in ipairs(st) do
 		if field.size == 1 then
 			data[field.name] = emu.read(addr + field.ofs, memType, field.signed)
 		elseif field.size == 2 then
@@ -172,8 +203,8 @@ local function st_monitor(st, instAddr, memType, ident)
 	mon.state = st_read(st, instAddr, memType, ident or "(mon)")
 	mon.changes = {}
 	mon.tick = function(self)
-		for i,field in ipairs(st) do
-			mon.changes[field.name] = {}
+		for _,field in ipairs(st) do
+			self.changes[field.name] = {}
 		end
 	end
 	mon.write = function(self, ifield, address, value)
@@ -257,7 +288,7 @@ local function st_fmt(obj, ident)
 	local out = ident
 	if st.fieldfmt then
 		out = out .. ":\n"
-		for i,field in ipairs(st) do
+		for _, field in ipairs(st) do
 			local s = st_fmt_field(st.fieldfmt[field.name], obj[field.name])
 			if s then
 				out = out .. string.format(" .%s: %s\n", field.name, s)
@@ -354,7 +385,96 @@ Rect.fieldfmt = {
 
 
 
+-------- Map Buffer Inspector --------
+
+local MapChunkSlot = {
+	SLOT_BUFFER = 0x0F,
+	SLOT_RENDERED = 0x10,
+	SLOT_NOCHUNK = 0x20,
+}
+
+MapChunkSlot.decode = function(data)
+	local t = {}
+	t.buffer = data & MapChunkSlot.SLOT_BUFFER
+	t.rendered = data & MapChunkSlot.SLOT_RENDERED ~= 0
+	t.nochunk = data & MapChunkSlot.SLOT_NOCHUNK ~= 0
+	return t
+end
+
+MapChunkSlot.read = function(address, memType)
+	local data = emu.read(address, memType)
+	return MapChunkSlot.decode(data)
+end
+
+MapChunkSlot.readArray = function(address, memType, count)
+	local out = {}
+	for _ = 1, count do
+		table.insert(out, MapChunkSlot.read(address, memType))
+	end
+	return out
+end
+
+local function readBytes(startAddress, memType, length, signed)
+	signed = signed or false
+	local addr = startAddress
+	local bytes = {}
+	for _ = 1, length do
+		local value = emu.read(addr, memType, signed)
+		table.insert(bytes, value)
+		addr = addr + 1
+	end
+	return bytes
+end
+
+local function getLabel(symbol)
+	local label = emu.getLabelAddress(symbol)
+	if not label then
+		return nil
+	end
+	label.symbol = symbol
+	return label
+end
+
+local labelsMapBufferChr = {}
+local labelsMapBufferAtrb = {}
+for i = 0, 8 do
+	table.insert(labelsMapBufferChr, getLabel(string.format("wMapBufferChr%d", i)))
+	table.insert(labelsMapBufferAtrb, getLabel(string.format("wMapBufferAtrb%d", i)))
+end
+
+MapTool = {
+	CHUNK_SIZE = 16 * 16,
+	CHUNK_SLOTS_COUNT = 9,
+	SCRATCH_SLOTS_COUNT = 3,
+
+	labels = {
+		slots = getLabel("hMapChunkSlots"),
+		chr_buffers = labelsMapBufferChr,
+		atrb_buffers = labelsMapBufferAtrb,
+	},
+
+	readState = function(self)
+		if self.labels.slots then
+			local l = self.labels.slots
+			self.slots = MapChunkSlot.readArray(l.address, l.memType, self.CHUNK_SLOTS_COUNT)
+		end
+
+		local chr_buffers = {}
+		for _, label in ipairs(self.labels.chr_buffers) do
+			table.insert(chr_buffers, readBytes(label.address, label.memType, self.CHUNK_SIZE))
+		end
+		self.chr_buffers = chr_buffers
+	end,
+}
+
+
+
 -------- Do stuff --------
+
+local memTypeNames = {}
+for k,v in pairs(emu.memType) do
+	memTypeNames[v] = k
+end
 
 local dottogrid = function(d)
 	return d >> 3
@@ -380,7 +500,7 @@ end
 
 local iAbsMax = function(t, max0)
 	local hi = max0
-	for i,x in ipairs(t) do
+	for _, x in ipairs(t) do
 		hi = absMax(hi or x, x)
 	end
 	return hi
@@ -397,6 +517,25 @@ local function onEndFrame()
 	overlay:select()
 	overlay:drawWindow()
 
+	MapTool:readState()
+	-- draw chunk cache
+	if MapTool.chr_buffers and #MapTool.chr_buffers == 9 then
+		local tile_size = 3
+		local chunkDispSize = 16 * tile_size
+		local _, dispH = overlay:getSize()
+		local originX = 0
+		local originY = dispH - chunkDispSize * 3
+		for cy = 0, 2 do
+			local py = originY + cy * chunkDispSize
+			for cx = 0, 2 do
+				local px = originX + cx * chunkDispSize
+				local idx = 1 + cy * 3 + cx
+				local chrs = MapTool.chr_buffers[idx]
+				overlay:drawTilemap(px, py, chrs, 16, 16, tile_size)
+			end
+		end
+	end
+
 --	local surfW = overlay:getSurfaceSize()
 --	for x = 0, surfW, 64 do
 --		overlay:drawString(x, -8, tostring(x))
@@ -409,14 +548,13 @@ local function onEndFrame()
 		local s = st_fmt(scroll)
 		local dy, dx
 		if monScroll then
-			dy = iAbsMax(monScroll.changes.dy)
-			dx = iAbsMax(monScroll.changes.dx)
+			dy = iAbsMax(monScroll.changes.dy, 0)
+			dx = iAbsMax(monScroll.changes.dx, 0)
 			s = s .. string.format(" dx,dy: %3d,%3d", dx, dy)
 			monScroll:tick()
 		end
 		overlay:drawString(px, py, s)
 		if dx then
-			local w,h = overlay:getSize()
 			overlay:drawVector(px + 16, py + 52 + 16, dx, dy, 15, 16, 0xEE80DD, 0xA0303030)
 		end
 	end
