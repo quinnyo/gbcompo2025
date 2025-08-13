2036bdf314bb3e46653cc33f410da4cac98f9dea
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-08-13 11:29:22 +1000

    maps/c1: begins

---
+++ src/assets/maps/enviro1.tsx
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="enviro1" tilewidth="8" tileheight="8" tilecount="128" columns="16">
+ <transformations hflip="1" vflip="1" rotate="0" preferuntransformed="1"/>
+ <image source="../enviro1.png" width="128" height="64"/>
+ <tile id="0" probability="5"/>
+ <tile id="1" probability="0.02"/>
+ <tile id="2" probability="0.02"/>
+ <tile id="3" probability="0.02"/>
+ <tile id="4" probability="0.02"/>
+ <tile id="5" probability="0.02"/>
+ <tile id="9" probability="0.01"/>
+ <tile id="10" probability="0.01"/>
+ <tile id="11" probability="0.01"/>
+ <tile id="12" probability="0.01"/>
+ <tile id="13" probability="0.01"/>
+ <tile id="14" probability="0.01"/>
+ <tile id="15" probability="0.01"/>
+ <tile id="33" probability="0.2"/>
+ <tile id="34" probability="0.2"/>
+ <tile id="35" probability="0.2"/>
+ <tile id="49" probability="0.2"/>
+ <tile id="50" probability="0.2"/>
+ <tile id="51" probability="0.2"/>
+ <tile id="65" probability="0.2"/>
+ <tile id="66" probability="0.2"/>
+ <tile id="67" probability="0.2"/>
+ <wangsets>
+  <wangset name="Grassy" type="mixed" tile="-1">
+   <wangcolor name="Open" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Solid" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="3,2,3,2,3,2,3,2"/>
+   <wangtile tileid="8" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="16" wangid="1,1,2,2,2,1,1,1"/>
+   <wangtile tileid="17" wangid="1,1,2,2,3,2,2,1"/>
+   <wangtile tileid="18" wangid="1,1,2,2,3,2,2,1"/>
+   <wangtile tileid="19" wangid="1,1,2,2,3,2,2,1"/>
+   <wangtile tileid="20" wangid="1,1,1,1,2,2,2,1"/>
+   <wangtile tileid="21" wangid="3,2,2,1,2,2,3,2"/>
+   <wangtile tileid="22" wangid="3,2,2,1,1,1,2,2"/>
+   <wangtile tileid="23" wangid="3,2,3,2,2,1,2,2"/>
+   <wangtile tileid="32" wangid="2,2,3,2,2,1,1,1"/>
+   <wangtile tileid="36" wangid="2,1,1,1,2,2,3,2"/>
+   <wangtile tileid="37" wangid="2,1,1,1,2,2,3,2"/>
+   <wangtile tileid="39" wangid="2,2,3,2,2,1,1,1"/>
+   <wangtile tileid="48" wangid="2,2,3,2,2,1,1,1"/>
+   <wangtile tileid="52" wangid="2,1,1,1,2,2,3,2"/>
+   <wangtile tileid="53" wangid="2,1,2,2,3,2,3,2"/>
+   <wangtile tileid="54" wangid="1,1,2,2,3,2,2,1"/>
+   <wangtile tileid="55" wangid="2,2,3,2,3,2,2,1"/>
+   <wangtile tileid="64" wangid="2,2,3,2,2,1,1,1"/>
+   <wangtile tileid="68" wangid="2,1,1,1,2,2,3,2"/>
+   <wangtile tileid="80" wangid="2,2,2,1,1,1,1,1"/>
+   <wangtile tileid="81" wangid="3,2,2,1,1,1,2,2"/>
+   <wangtile tileid="82" wangid="3,2,2,1,1,1,2,2"/>
+   <wangtile tileid="83" wangid="3,2,2,1,1,1,2,2"/>
+   <wangtile tileid="84" wangid="2,1,1,1,1,1,2,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
