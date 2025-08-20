fa6dce40501e9c4337127cff10f58f6f29a14319
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-08-20 16:43:37 +1000

    Maps/tilesets: go stupid simple mode
    - enviro0 tileset: minimal auto-tiling, 2-tone it'll do
    - convert maps/c1 to enviro0

---
+++ src/assets/maps/enviro0.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="enviro0" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="../enviro0.png" width="64" height="64"/>
+ <wangsets>
+  <wangset name="It" type="corner" tile="-1">
+   <wangcolor name="Solid" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Open" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="2" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="3" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="4" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="17" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="18" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="19" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="20" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="21" wangid="0,1,0,2,0,1,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
