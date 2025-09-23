32965d91e571c07a2ca1943f34b3c8debff6f001
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-09-24 06:34:06 +1000

    Maps: centre-align Items tile objects.

--- src/assets/items.tsx
+++ src/assets/items.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="items" tilewidth="8" tileheight="8" tilecount="16" columns="16">
+<tileset version="1.10" tiledversion="1.11.2" name="items" tilewidth="8" tileheight="8" tilecount="16" columns="16" objectalignment="center">
  <image source="ent/items.png" width="128" height="8"/>
 </tileset>
