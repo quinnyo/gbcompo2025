35eb7b09501846c7895f187be9bd531ea7b7da63
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-10-18 00:44:53 +1000

    Remove old test map and unused tilesets.

--- src/assets/mini.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="mini" tilewidth="8" tileheight="8" tilecount="4" columns="2">
- <transformations hflip="1" vflip="1" rotate="0" preferuntransformed="1"/>
- <image source="mini.png" width="16" height="16"/>
-</tileset>
