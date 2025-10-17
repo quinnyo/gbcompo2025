9ab5a87b3e8914c032feeb4d4b0b842edb816327
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-10-18 00:49:43 +1000

    Remove editor tileset.

--- src/assets/editor.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="editor" tilewidth="8" tileheight="8" tilecount="64" columns="8">
- <properties>
-  <property name="editor_only" type="bool" value="true"/>
- </properties>
- <image source="editor.png" width="64" height="64"/>
-</tileset>
