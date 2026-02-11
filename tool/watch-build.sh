9770a5282837c43b2ee6312391eb95f25a56b1e0
Quinn <3379314+quinnyo@users.noreply.github.com>
2026-02-11 14:00:13 +1000

    Various fixes for tool/watch-build.sh
    - address shellcheck lints
    - refactor exclude pattern to begin as an array before combining (readability)
    - add rust 'target' dirs to excludes. prevents spurious rebuilds and related issues when working on mapconv.

--- tool/watch-build.sh
+++ tool/watch-build.sh
@@ -2,14 +2,16 @@
 set -euo pipefail
 
 function logecho {
-	printf "[%s] $@\n" "$(date --iso-8601=seconds)"
+	printf "%s " "[$(date --iso-8601=seconds)]" "$@"
+	printf "\n"
 }
 
 function errorecho {
-	logecho "ERROR: $@" >&2
+	logecho "ERROR:" "$@" >&2
 }
 
 function errorexit {
+	local retval
 	retval=1
 	if [ $# -gt 0 ]; then
 		if [[ $1 =~ [0-9]+ ]]; then
@@ -22,19 +24,20 @@ function errorexit {
 	else
 		errorecho "?"
 	fi
-	exit $retval
+	exit "$retval"
 }
 
 function requirecmd {
+	local status
 	status=0
-	while [ $# -gt 0 ]; do
-		type "${1}" || (
+	while [[ $# -gt 0 ]]; do
+		if ! type "${1}"; then
 			errorecho "command '${1}' not found."
 			status=1
-		)
+		fi
 		shift
 	done
-	[ $status -eq 0 ] || exit $status
+	return $status
 }
 
 function projectdir_fingerprint_data {
@@ -57,17 +60,31 @@ function build {
 	fi
 }
 
-requirecmd inotifywait redo
+requirecmd inotifywait redo || exit 1
+
 
 ## File events to monitor in order to trigger rebuild
 EVENTS=(modify create delete delete_self move move_self)
 ## Files and directories to watch
-MONITOR=(inc src tool '*.do' '*.sh')
-## Pattern to match filenames to exclude from monitoring
-MONITOR_EXCLUDE='.*([.](tiled-project|tiled-session|tmx|tsx)[.][^.]*|~$)'
+MONITOR=(inc src tool *.do *.sh)
+## Patterns to match filenames to exclude from monitoring
+_excludes=(
+	# files (including dirs) beginning with a dot
+	'/[.]'
+	# probable temporary/backup files
+	'([.](orig|bak)|~)$'
+	# Tiled temporary files
+	'[.](tiled-project|tiled-session|tmx|tsx)[.][^.]+'
+	# Rust build dir
+	'/target/'
+)
+# join exclude patterns into one super pattern
+_excljunc="$(printf "(%s)|" "${_excludes[@]}")"
+_excljunc="${_excljunc%?}"
+MONITOR_EXCLUDE="${_excljunc}"
 
 # make comma separated list
-printf -v events_list '%s,' ${EVENTS[@]}
+printf -v events_list '%s,' "${EVENTS[@]}"
 events_list=${events_list%,}
 
 PROJECT_ID="$(projectdir_fingerprint)"
@@ -75,7 +92,7 @@ PROJECT_STATE="${XDG_STATE_HOME:-$HOME/.local/state}/watch-build/${PROJECT_ID}"
 mkdir -p "${PROJECT_STATE}"
 EVENT_FILE="$(mktemp -p "${PROJECT_STATE}/" eventsXXXX)"
 
-inotifywait --monitor --outfile "${EVENT_FILE}" --recursive --event ${events_list} --exclude "${MONITOR_EXCLUDE}" ${MONITOR[@]} &
+inotifywait --monitor --outfile "${EVENT_FILE}" --recursive --event "${events_list}" --exclude "${MONITOR_EXCLUDE}" "${MONITOR[@]}" &
 MONITOR_PID=$!
 
 trap "logecho 'Stop signal received.' && exit" SIGINT SIGTERM
